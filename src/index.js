d5be62703b4b87759a96771b79a1932bc52ec56f
KainanPR <kainanpr95@gmail.com>
2019-11-13 00:18:04 -0300

    Checking internet connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  View,
+} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+import CustomModal from './components/modal/customModal';
+
 import Routes from './routes';
 
-const App = () => <Routes />;
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalVisible: false,
+    };
+
+    setTimeout(() => {
+      NetInfo.isConnected.addEventListener('connectionChange', (res) => {
+        if (res === false) {
+          this.setState({ modalVisible: true });
+        }
+      });
+    }, 2000);
+  }
+
+  setModalVisible = (modalVisible) => {
+    this.setState({ modalVisible });
+  }
+
+  render() {
+    return (
+      <>
+        <Routes />
+        <CustomModal
+          visible={this.state.modalVisible}
+          onButtonPress={() => {
+            this.setModalVisible(false);
+          }}
+          text="Sem conexão com a Internet!"
+          hasError
+        />
+      </>
+    );
+  }
+}
 
 export default App;
